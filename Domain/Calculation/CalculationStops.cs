2de251cb986693ecfc6a4543c0307f59154537f3
RIBEIRO.DSN.PACK <nandobcr@gmail.com>
2020-11-24 08:17:22 -0300

    Created calculation stops required (TDD)

---
+++ Domain/Calculation/CalculationStops.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Domain.Calculation
+{
+    public class CalculationStops
+    {
+        private const int Hour = 24; 
+
+        public int CalculateStops(int distance, int mglt, int daysCount)
+        {
+            int autonomy = (daysCount * Hour * mglt);
+            return (autonomy > 0) ? (distance / autonomy) : 0;
+        }
+    }
+}
--- Test/App/CalculationStopsTest.cs
+++ Test/App/CalculationStopsTest.cs
@@ -1,10 +1,48 @@
 using Xunit;
-using System;
+using Domain.Calculation;
 
 namespace Test.App
 {
     public class CalculationStopsTest
     {
+        [Fact]
+        public void ShouldReturn9StopsWhenTheDistanceEqual1MillionAndMGLTEqual75AndConsumablesEqual60Days()
+        {
+            var calculationStops = new CalculationStops();
+            int stopsRequired = calculationStops.CalculateStops(1000000, 75, 60);
+            Assert.Equal(9, stopsRequired);
+        }
 
+        [Fact]
+        public void ShouldReturn11StopsWhenTheDistanceEqual3MillionsAndMGLTEqual10AndConsumablesEqual1095Days()
+        {
+            var calculationStops = new CalculationStops();
+            int stopsRequired = calculationStops.CalculateStops(3000000, 10, 1095);
+            Assert.Equal(11, stopsRequired);
+        }
+
+        [Fact]
+        public void ShouldReturn5StopsWhenTheDistanceEqual100HundredThousandAndMGLTEqual100AndConsumablesEqual7Days()
+        {
+            var calculationStops = new CalculationStops();
+            int stopsRequired = calculationStops.CalculateStops(100000, 100, 7);
+            Assert.Equal(5, stopsRequired);
+        }
+
+        [Fact]
+        public void ShouldReturn0StopsWhenTheDistanceEqual100HundredThousandAndMGLTEqual0AndConsumablesEqual7Days()
+        {
+            var calculationStops = new CalculationStops();
+            int stopsRequired = calculationStops.CalculateStops(100000, 0, 7);
+            Assert.Equal(0, stopsRequired);
+        }
+
+        [Fact]
+        public void ShouldReturn0StopsWhenTheDistanceEqual100HundredThousandAndMGLTEqual150AndConsumablesEqual0Days()
+        {
+            var calculationStops = new CalculationStops();
+            int stopsRequired = calculationStops.CalculateStops(100000, 150, 0);
+            Assert.Equal(0, stopsRequired);
+        }        
     }
 }
